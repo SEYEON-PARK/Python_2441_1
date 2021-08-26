869e0ffd3a1c43f365696b20c5fe4007e3edc35a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-08-27 00:04:24 +0900

    Python언어 BAEKJOON 2441번 문제 풀기!(1)

--- Python_2441_1.py
+++ Python_2441_1.py
@@ -10,8 +10,8 @@
 N=int(input()) # 사용자로부터 정수 입력받기
 
 for i in range(N, 0, -1): # i에 N부터 0이 되기 전까지 -1씩 하며 반복 
-    for j in range(N-i):
+    for j in range(N-i): # j에 0부터 N-i이 되기 전까지 1씩 더하며 반복
         print(" ", end="") # 공백 출력하기
-    for k in range(i):
+    for k in range(i): # k에 0부터 i가 되기 전까지 1씩 더하며 반복
         print("*", end="") # 별 
-    print()
+    print() # 한줄 띄기
